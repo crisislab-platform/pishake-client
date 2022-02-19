800548e6ef4b04b050d7ef1c63508fcec389eec6
Zade Viggers <74938858+zadeviggers@users.noreply.github.com>
2022-02-19 19:19:27 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
 (crontab -l 2>/dev/null; echo "@reboot node /opt/pishake-client/script.js") | crontab -
+npm i -g pnpm
+pnpm i
