9428a53b8db4230e1378d5320f5b96689a9ac2a9
ben476 <benjamin.hong476@gmail.com>
2022-04-30 13:08:48 +1200

    only setup plum now

--- setup_old.sh
+++ setup_old.sh
@@ -25,22 +25,29 @@ fi
 echo Installing dependencies
 apt-get remove gpsd gpsd-clients python-gps -y
 apt update -y
-apt install git curl
+apt install git
+
+echo Adding Node.js source
+curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+echo Installing Node.js
+apt-get install -y nodejs
 
 echo Downloading client
 rm -rf /opt/pishake-client
-git clone https://github.com/rs-Web-Interface-CRISiSLab/Decentralised_Processing_PLUM_V1.0.git /opt/plum-client
-cd /opt/plum-client
+git clone https://github.com/rs-Web-Interface-CRISiSLab/pishake-client.git /opt/pishake-client
+cd /opt/pishake-client
 
-echo Installing Decentralised PLUM Algorithm
-bash setup.sh
+echo Installing dependencies
+npm install
+
 
 # Install as service
-# echo Installing service
-# sed -i 's/<token>/'$CRISISLAB_SENSOR_TOKEN'/g' /opt/pishake-client/pishake-client.service
-# cp /opt/pishake-client/pishake-client.service /etc/systemd/system/pishake-client.service
-# systemctl daemon-reload
+echo Installing service
+sed -i 's/<token>/'$CRISISLAB_SENSOR_TOKEN'/g' /opt/pishake-client/pishake-client.service
+cp /opt/pishake-client/pishake-client.service /etc/systemd/system/pishake-client.service
+systemctl daemon-reload
 # systemctl enable pishake-client.service
 # systemctl start pishake-client.service
 
-echo Done\! Your sensor should now be part of our network.
+echo Done\! Your sensor should now be streaming data to the server.
